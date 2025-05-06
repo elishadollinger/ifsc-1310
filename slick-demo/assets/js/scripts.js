a33619755071a844528ebc885b69f27821226dce
elishadollinger <erdollinger@ualr.edu>
2025-05-06 16:48:46 +0000

    implementing my own work

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     $('.fade').slick({
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 250,
       fade: true,
       slide: 'div',
       cssEase: 'linear',
       autoplay: true,
-      autoplaySpeed: 2000
+      autoplaySpeed: 500
     });
 
 
